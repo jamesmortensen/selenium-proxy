07edf4a9a07cf84a23e3bc531078f97418c2112f
James Mortensen <jmort253@gmail.com>
2022-10-15 11:43:55 +0530

    using node:path to copy the default-config when generating a new config. This helps make it more cross-platform as well as can potentially create a binary with vercel's pkg tool.

--- src/config-generator.js
+++ src/config-generator.js
@@ -1,9 +1,10 @@
 // config-generator.js
 
+const path = require('node:path');
 const fs = require('fs');
 if (fs.existsSync(process.cwd() + '/config.json'))
     console.log('Config file config.json already present. Rename or delete to create a new one.');
 else {
     console.log('Generate a config file proxy-config.json in ' + process.cwd());
-    fs.copyFileSync(__dirname + '/../default-config.json', process.cwd() + '/config.json')
+    fs.copyFileSync(path.join(__dirname, "../default-config.json"), process.cwd() + '/config.json');
 }
